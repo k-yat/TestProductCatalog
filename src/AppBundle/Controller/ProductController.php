fb37251f520e07f2dc6c70fd260c1871b4901898
Katya Yatsenco <k.yatsenco@gmail.com>
2018-05-12 08:58:49 +0300

    Frontend: Implement main page with list of all products.

--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -9,6 +10,8 @@ use Symfony\Component\HttpFoundation\Request;
 
 class ProductController extends Controller
 {
+    public const PRODUCT_COUNT_PER_PAGE = 4;
+
     /**
      * @var EntityManagerInterface
      */
@@ -24,7 +27,17 @@ class ProductController extends Controller
      */
     public function showAllAction(Request $request)
     {
+        $productsQuery = $this->entityManager->getRepository(Product::class)->getProductsQuery();
+
+        $paginator = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $productsQuery,
+            $request->query->getInt('page', 1),
+            self::PRODUCT_COUNT_PER_PAGE
+        );
+
         return $this->render('product/show_all.html.twig', [
+            'pagination' => $pagination
         ]);
     }
 }
--- src/AppBundle/Repository/ProductRepository.php
+++ src/AppBundle/Repository/ProductRepository.php
@@ -2,6 +2,13 @@
 
 namespace AppBundle\Repository;
 
+use Doctrine\ORM\Query;
+
 class ProductRepository extends \Doctrine\ORM\EntityRepository
 {
+    public function getProductsQuery(): Query
+    {
+        return $this->getEntityManager()
+            ->createQuery('Select p From AppBundle:Product p');
+    }
 }
